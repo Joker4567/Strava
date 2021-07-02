ac7f2cfd859622bf8b1aa39f26a4a01b191f144a
danufriyev <W5n@$0scByt4>
2021-07-02 18:06:23 +0500

    test commit contribute

--- app/src/main/java/com/skillbox/strava/di/RoomModule.kt
+++ app/src/main/java/com/skillbox/strava/di/RoomModule.kt
@@ -3,8 +3,7 @@ package com.skillbox.strava.di
 import android.content.Context
 import com.skillbox.core_db.room.SkillboxDatabase
 import com.skillbox.core_db.room.dao.AthleteDao
-import dagger.Module
-import dagger.Provides
+import dagger.*
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
